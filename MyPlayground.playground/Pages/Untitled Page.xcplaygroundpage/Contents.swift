3050b3dcf503bbced61376da2fa7fa588687d99f
Sarah Borque <sarahandoctopus@Sarahs-MacBook-Air.local>
2016-06-20 15:12:04 -0400

    Initial Comit

---
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+var str = "Hello, playground"
+
+
+str = "Hello World"
+
+str
+var number = 10
+number = 20
+
+
+
+var string = "Mason"
+var stringTwo = "Sarah"
+var completeString = string + " and " + stringTwo
+
+
+var x = 20
+var y = 10
+
+var answer = x + y
+
+answer += answer
+
---
+++ MyPlayground.playground/Pages/operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+//: [Previous](@previous)
+
+
+// Operator Pecedence
+
+var x = 100 + 100 - 5 * 2 / 3 % 7
+
+let y = 25 - 5 * 2 + 5
+
+let a = (2 + 2) * 2 + 2
+
+
+// Unary Operators
+
+var levelScore = 0
+levelScore = levelScore + 1
+
+var totalScore = levelScore
+
+totalScore = ++levelScore
+
+totalScore = --levelScore
+
+totalScore = totalScore + 5
+
+totalScore += 5
+
+
+// NOT Operatore
+
+let on = true
+let off = !on
+
+1 != 2
+
+// Enter your code below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//: [Next](@next)
